c3067c8b3beada7a499c8a9f600659547f84b069
Abdullah272056 <abdullah272056@gmail.com>
2020-02-20 00:09:12 +0600

    no change

--- app/src/main/java/com/example/customalertdialog/MainActivity.java
+++ app/src/main/java/com/example/customalertdialog/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
                         alertDialog.cancel();
                     }
                 });
-                alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+               // alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
 
                 alertDialog.show();
 
